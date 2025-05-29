c7128d37712e4228b789a7317709c9511d68f7fd
GuiPadoDevs <guilherme.padovine@wemov.com.br>
2025-05-29 12:34:42 -0300

    adjusts

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,9 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
+  windowMs: 15 * 60 * 1000,
   max: 100,
   message: 'Muitas requisições. Tente novamente mais tarde.',
+  keyGenerator: (req) => req.ip,
 });
 
 module.exports = limiter;
